5cd9a5b9d35842c242e8a1d527b8010a1154281d
Thales Henrique <thaalesheenrique@gmail.com>
2020-03-26 15:34:53 -0300

    Adicionando Session Login da Ong

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,11 @@ const routes  = express.Router();
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+routes.post('/sessions', SessionController.create)
 
 routes.get('/ongs', OngController.index);
-
 routes.post('/ongs', OngController.create);
 
 routes.get('/profile', ProfileController.index);
